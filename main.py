052c9d6f4205797bd7baab4e5d3eb9875eba658c
demimolchabite <traorefanogo@gmail.com>
2024-08-10 16:45:05 +0100

    little change

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from verifier import verifier
 from supprimer import supprimer
 from afficher import afficher
 def main ():
-    """user = compte()
+    user = compte()
     user.saisir_save_user()
-    code = input("Entrez votre mot de passe : ")
+    """code = input("Entrez votre mot de passe : ")
     name = input("Entrez votre nom : ")
     surname = input("Entrer votre prenom : ")
     mes_listes = ouvrir_liste() 
     verifier (code,name,surname,mes_listes)"""
-    supprimer ()
+    #supprimer ()
     afficher()
 
 if __name__ == "__main__":
